d5ec372e667737944fc2844de9235a7774d17c4d
Bomi Choi <hibomi97@gmail.com>
2021-09-13 05:33:33 +0900

    Add createAt & updatedAt

---
+++ prisma/migrations/20210912194416_created_at_updated_at/migration.sql
@@ -0,0 +1,20 @@
+/*
+  Warnings:
+
+  - Added the required column `updatedAt` to the `CoffeeShopPhoto` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "Category" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP;
+
+-- AlterTable
+ALTER TABLE "CoffeeShop" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP;
+
+-- AlterTable
+ALTER TABLE "CoffeeShopPhoto" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
+
+-- AlterTable
+ALTER TABLE "User" ALTER COLUMN "updatedAt" SET DEFAULT CURRENT_TIMESTAMP;
--- src/coffeeshops/seeCoffeeShops/seeCoffeeShops.resolvers.js
+++ src/coffeeshops/seeCoffeeShops/seeCoffeeShops.resolvers.js
@@ -4,6 +4,9 @@ export default {
     Query: {
         seeCoffeeShops: async (_, { offset }) => {
             const shops = await client.coffeeShop.findMany({
+                orderBy: {
+                    createdAt: "desc",
+                },
                 take: 5,
                 skip: offset ? offset : 0,
             });
